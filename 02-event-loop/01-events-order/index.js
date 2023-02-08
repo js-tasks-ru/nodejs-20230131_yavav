c3033f47752d86338fde46caa977e1a0dfe825e1
Alexey V <yavav@yandex.ru>
2023-02-08 22:20:23 +0300

    2nd task 2

--- 02-event-loop/01-events-order/index.js
+++ 02-event-loop/01-events-order/index.js
@@ -1,50 +1,23 @@
-const fs = require('fs');
-// не понял (
-const myFile = 'solution.txt';
-const myFullPath = myFile;
-
-function myFileDelCrt(myFullPath) {
-    if (fs.existsSync(myFullPath)) {
-        try {
-            fs.unlinkSync(myFullPath);
-            fs.writeFileSync(myFullPath, '','utf8');
-          } catch (err) {
-            console.log(err);
-          }
-    }
-}
-
-function myFileAddInto(myFullPath, myAddStr) {
-  fs.appendFile(myFullPath, myAddStr,'utf8', err => {if (err) throw err;})
-}
-
-myFileDelCrt(myFullPath);
-
 const intervalId = setInterval(() => {
   console.log('James');
-  myFileAddInto(myFullPath, 'James\n');
 }, 10);
 
 setTimeout(() => {
   const promise = new Promise((resolve) => {
     console.log('Richard');
-    myFileAddInto(myFullPath, 'Richard\n');
     resolve('Robert');
   });
 
   promise
       .then((value) => {
         console.log(value);
-        myFileAddInto(myFullPath, value + '\n');
 
         setTimeout(() => {
           console.log('Michael');
-          myFileAddInto(myFullPath, 'Michael');
 
           clearInterval(intervalId);
         }, 10);
       });
 
   console.log('John');
-  myFileAddInto(myFullPath, 'John\n');
 }, 10);
