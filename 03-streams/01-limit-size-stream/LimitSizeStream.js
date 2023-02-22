7bdac18919e3dd7715a0f2ab2276b8b8a0240ef3
Alexey V <yavav@yandex.ru>
2023-02-22 21:14:49 +0300

    Task 3-1-1 StreamSize

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.setEncoding(options.encoding);
+    // this.curSize = 0;
+    this.curSize = '';
   }
 
   _transform(chunk, encoding, callback) {
-
+    // if (this.curSize + Buffer.from(chunk, encoding).length > this.limit) {
+    if (Buffer.from(this.curSize + chunk).length > this.limit) {
+      console.log(new LimitExceededError);
+    } else {
+      callback(null, chunk);
+      // this.curSize += Buffer.from(chunk, encoding).length;
+      this.curSize += chunk;
+    }
   }
 }
 
