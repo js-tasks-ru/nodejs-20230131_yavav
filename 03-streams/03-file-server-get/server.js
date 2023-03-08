129995cf2e5276eda52445ca973c157820700b18
Alexey V <yavav@yandex.ru>
2023-03-08 15:29:45 +0300

    3-3 1st att

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -2,23 +2,48 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 
+const fs = require('fs');  
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const myStream = fs.createReadStream(filepath);
 
   switch (req.method) {
     case 'GET':
+      myStream.pipe(res);
+      // myStream.on('data', (chunk) => {
+      //   res.write(chunk);
+      // });
+      // myStream.on('end', () => {
+      //   res.end();
+      // });
+
+      myStream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        } else if (path.relative(filepath, path.join(__dirname, 'files')) != '') {
+          res.statusCode = 400;
+          res.end('Path is wrong');
+        }
+      });
 
       break;
 
     default:
-      res.statusCode = 501;
+      res.statusCode = 500;
       res.end('Not implemented');
   }
+  
+  req.on('aborted', () => {
+    res.destroy();
+  });
+
 });
 
 module.exports = server;
