19e08a0c2ad342e485cddeb637ab72aee73a06b4
Alexey V <yavav@yandex.ru>
2023-03-21 22:53:12 +0300

    3-2 1st att

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,13 +4,37 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    
+    this.lines = [];
+    this.myDelimit = '\r\n';
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, encoding, cb) {
+
+    while (true) {
+      let myPos = chunk.toString().indexOf(this.myDelimit, 0);
+
+      if (myPos < 0) break;
+
+      this.lines.push(chunk.toString().slice(0, myPos).trim());
+      chunk = Buffer.from(chunk.toString().slice(myPos + (this.myDelimit == '\r\n' ? 2: 1)));
+      
+    }
+    
+    if (chunk.toString().length >= 0) this.lines.push(chunk.toString().trim());
+    
+    cb();
   }
 
-  _flush(callback) {
-  }
+  _flush(cb) { 
+      this.lines.forEach((elm) => {
+        this.push(elm + this.myDelimit);
+      //cb(this.lines);
+      //cb();
+       });
+       cb();
+    };
+
 }
 
 module.exports = LineSplitStream;
